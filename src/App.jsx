969fc353fff6c0dc7aaabd46ed37bbd71af941a4
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-01-21 00:46:54 +0700

    complete create, edit and delete function

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,37 @@ import AppFooter from './layouts/AppFooter';
 import SunIcon from './assets/sun-icon.png';
 import MoonIcon from './assets/moon-icon.png';
 
-import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import ArchivePageWrapper from './pages/ArchivePage';
 import DetailPage from './pages/DetailPage';
 import NotePageWrapper from './pages/NotePage';
+import LoaderScreen from './components/ui/LoaderScreen';
 
-import { getNotes, getTheme, saveTheme } from './utils/local-storage';
+import { GetNotes, GetTheme, SaveTheme } from './utils/local-storage';
+import { AddNote, DeleteNote, EditNotes } from './utils/data-notes';
+
+import { ToastContainer, toast } from 'react-toastify';
+import CONFIG from './utils/config';
 
 class App extends React.Component {
   constructor(props) {    
     super(props);
 
-    const currentIcon = getTheme() === 'light' ? SunIcon : MoonIcon;
+    const currentIcon = GetTheme() === 'light' ? SunIcon : MoonIcon;
 
     this.state = {
+      notes: GetNotes(),
+      isLoadingTime: false,
       iconTheme: currentIcon,
-      currentTheme: getTheme(),
-      notes: getNotes(),
+      currentTheme: GetTheme(),
     };
 
     this.onToggleThemeHandler = this.onToggleThemeHandler.bind(this);
+    this.onLoadingScreenHandler = this.onLoadingScreenHandler.bind(this);
+
+    this.onSaveNoteHandler = this.onSaveNoteHandler.bind(this);
+    this.onEditNoteHandler = this.onEditNoteHandler.bind(this);
+    this.onDeleteNoteHandler = this.onDeleteNoteHandler.bind(this);
   }
 
   onToggleThemeHandler() {
@@ -35,13 +45,49 @@ class App extends React.Component {
       currentTheme: prevState.currentTheme === 'light' ? 'dark' : 'light',
       iconTheme: prevState.iconTheme === SunIcon ? MoonIcon : SunIcon,
     }), () => {
-      saveTheme(this.state.currentTheme);
+      SaveTheme(this.state.currentTheme);
+    });
+  }
+
+  onLoadingScreenHandler(){
+    this.setState((prevState) => ({
+      isLoadingTime: !prevState.isLoadingTime
+    }));
+  }
+
+  onSaveNoteHandler(note) {
+    AddNote(note);
+    toast.success('Successfuly created a new note', CONFIG.TOAST_EMITTER);
+
+    this.setState(() => {
+      return { notes: GetNotes() };
+    });
+  }
+
+  onEditNoteHandler(note) {
+    EditNotes(note);
+    toast.success('Successfuly updated the note', CONFIG.TOAST_EMITTER);
+
+    this.setState(() => {
+      return { notes: GetNotes() };
+    });
+  }
+
+  onDeleteNoteHandler(id) {
+    DeleteNote(id);
+    toast.success('Successfuly deleted the note', CONFIG.TOAST_EMITTER);
+
+    this.setState(() => {
+      return { notes: GetNotes() };
     });
   }
 
   render() {
     return (
       <div className={`app ${this.state.currentTheme}`}>
+        {this.state.isLoadingTime && (
+          <LoaderScreen />
+        )}
 
         <header className='app__header'>
           <AppNavbar 
@@ -52,18 +98,27 @@ class App extends React.Component {
   
         <main className='app__content'>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-
-            <Route path="/note" element={
-              <NotePageWrapper notes={this.state.notes} /> 
-            }/>
+            <Route path="/" element={
+              <NotePageWrapper 
+                notes={this.state.notes}
+                onSaveNote={this.onSaveNoteHandler}
+                onEditNote={this.onEditNoteHandler}
+                onDeleteNote={this.onDeleteNoteHandler}
+                onLoading={this.onLoadingScreenHandler}
+              /> 
+            } />
 
             <Route path="/archive" element={
-              <ArchivePageWrapper notes={this.state.notes} />
+              <ArchivePageWrapper 
+                notes={this.state.notes}
+                onEditNote={this.onEditNoteHandler}
+                onDeleteNote={this.onDeleteNoteHandler}
+                onLoading={this.onLoadingScreenHandler}
+              />
             }/>
 
             <Route path="/note/:id" element={
-              <DetailPage notes={this.state.notes} />
+              <DetailPage notes={this.state.notes}/>
             } />
 
             <Route path="/not-found" element={<NotFoundPage />} />
@@ -74,6 +129,8 @@ class App extends React.Component {
         <footer className='app__footer'>
           <AppFooter />
         </footer>
+
+        <ToastContainer style={{ maxWidth: '400px', width: '90%' }} />
       </div>
     );
   }
